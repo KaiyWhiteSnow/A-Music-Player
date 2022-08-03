6cbd650a96dc839aeb36b82ff8462b0a26eccf8d
KaiyWhiteSnow <106684837+KaiyWhiteSnow@users.noreply.github.com>
2022-08-03 13:51:31 +0200

    Add files via upload

---
+++ MP3PlayerV3/MP3PlayerV3/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MP3PlayerV3
+{
+    public partial class Form1 : Form
+    {
+        //Creating player
+        public System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+
+        //Connection between code and visuals
+        public Form1()
+        {    
+            InitializeComponent();
+        }
+
+        //songs 1-3
+        private void button1_Click(object sender, EventArgs e)
+        {
+            player.SoundLocation = @"C:\Users\jarda\source\repos\MP3PlayerV3\musicBox\music\M1.wav";
+            player.Play();            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            player.SoundLocation = @"C:\Users\jarda\source\repos\MP3PlayerV3\musicBox\music\M2.wav";
+            player.Play();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            player.SoundLocation = @"C:\Users\jarda\source\repos\MP3PlayerV3\musicBox\music\M3.wav";
+            player.Play();
+        }
+
+        //stop button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+    }  
+}
